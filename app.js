acd90bf67d2a58dee520661d7f7fdef35cd4104a
Ayoub Nejjari <raysoflee07@gmail.com>
2017-05-19 23:59:17 +0100

    adding post routes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 
 let app = express();
@@ -7,7 +8,7 @@ let app = express();
 // set static path
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+app.use(bodyParser.urlencoded({extended: false}));
 // define routes
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
@@ -23,6 +24,13 @@ app.get('/contact', function (req, res) {
 
 app.get('/info', function(req, res) {
     res.download('public/sample.pdf');
+});
+
+app.post('/subscribe', function(req, res) {
+  let name = req.body.name;
+  let email = req.body.email;
+  console.log(name + ' has been subscribed with the email: ' + email);
+  res.redirect('/index.html');
 });
 
 
